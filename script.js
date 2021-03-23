891942fb39393714401d91efa5b5f6286fb25bff
Emily Jette <emily.jette@ocsbstudent.ca>
2021-03-23 15:04:06 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// This code decides if the number the user inputs is positive or negative and delivers a message accordingly
+
+document.getElementById('button').addEventListener('click', comparison)
+let userNumber = 0
+
+function comparison () {
+  // convert userNumber into integer
+  userNumber = document.getElementById('input').value
+  userNumber = parseInt(userNumber)
+  
+  if (userNumber < 0){
+    document.getElementById('answer').innerHTML = 'The number you wrote was negative'
+  }else{
+    document.getElementById('answer').innerHTML = 'The number you wrote was positive'
+  }
+}
